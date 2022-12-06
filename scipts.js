c6d38ba02497efeb6b289b97c94109766aa51e73
severus <frayseverus@gmail.com>
2022-12-06 19:46:00 -0300

    Fusión del menú en mobile

--- scipts.js
+++ scipts.js
@@ -1,9 +1,19 @@
 const dv_desktopMenu = document.querySelector('.desktop-menu');
 const li_navbarEmail = document.querySelector('.navbar-email');
+const dv_mobileMenu = document.querySelector('.mobile-menu');
+const btn_menuMobile = document.querySelector('#btn_menuMobile');
+
 
 li_navbarEmail.addEventListener('click', fnctn__desktopMenu__toggleInactive);
+btn_menuMobile.addEventListener('click', fnctn__menuMobile__toggleInactive);
+
 
 function fnctn__desktopMenu__toggleInactive(){
     console.log('fnctn__desktopMenu__toggleInactive');
     dv_desktopMenu.classList.toggle('inactive');
+}
+
+function fnctn__menuMobile__toggleInactive() {
+    console.log('fnctn__menuMobile__toggleInactive');
+    dv_mobileMenu.classList.toggle('inactive');
 }
